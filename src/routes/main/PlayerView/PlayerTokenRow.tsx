35e1a39033fee633d1c167c48e793f939696b73d
jacksontoomey <jacksontoomey@gmail.com>
2023-09-20 15:36:35 -0500

    Cleanup

--- src/routes/main/PlayerView/PlayerTokenRow.tsx
+++ src/routes/main/PlayerView/PlayerTokenRow.tsx
@@ -1,12 +1,23 @@
-import { getInitiativeMetadata, useToken } from "../../../hooks/owlbear";
+import {
+  getInitiativeMetadata,
+  useScene,
+  useToken,
+} from "../../../hooks/owlbear";
 import TokenName from "./TokenName";
 import TokenStatus from "./TokenStatus";
 
 export default function PlayerTokenRow({ tokenId }: { tokenId: string }) {
   const [token] = useToken(tokenId);
+  const [scene] = useScene();
   const metadata = getInitiativeMetadata(token);
   return (
-    <tr>
+    <tr
+      className={
+        scene.inCombat && scene.currentInitiativeToken === tokenId
+          ? "bg-primary"
+          : ""
+      }
+    >
       <td>{metadata?.initiativeCount || 0}</td>
       <td className="whitespace-normal">
         <TokenName token={token} />
--- src/routes/main/PlayerView/index.tsx
+++ src/routes/main/PlayerView/index.tsx
@@ -4,6 +4,7 @@ import PlayerTokenRow from "./PlayerTokenRow";
 export default function PlayerView() {
   const [scene] = useScene();
   const tokenIds = useTokenIds();
+  console.log(scene);
   if (!scene.inCombat) return <div>Not in combat</div>;
   return (
     <div>
@@ -17,7 +18,7 @@ export default function PlayerView() {
         </thead>
         <tbody>
           {tokenIds.map((tokenId) => (
-            <PlayerTokenRow tokenId={tokenId} />
+            <PlayerTokenRow key={tokenId} tokenId={tokenId} />
           ))}
         </tbody>
       </table>
