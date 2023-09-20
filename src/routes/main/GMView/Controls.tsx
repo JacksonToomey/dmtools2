eebd90325e3e026db6489e1991c0f6e807dae08a
jacksontoomey <jacksontoomey@gmail.com>
2023-09-20 15:48:07 -0500

    round time

--- src/routes/main/GMView/Controls.tsx
+++ src/routes/main/GMView/Controls.tsx
@@ -1,3 +1,5 @@
+import { formatDuration, intervalToDuration } from "date-fns";
+import { useEffect, useRef, useState } from "react";
 import {
   resetInitiative,
   setNextToken,
@@ -7,16 +9,31 @@ import {
 
 export default function Controls() {
   const [scene, setScene] = useScene();
+  const start = useRef(new Date());
   const ids = useTokenIds();
+  const [currentDate, setCurrentDate] = useState(new Date());
   const handleCombatToggle = () => {
     const inCombat = !scene?.inCombat;
     setScene({ inCombat, rounds: 1, currentInitiativeToken: ids[0] });
-    if (!inCombat) resetInitiative(ids);
+    if (!inCombat) {
+      resetInitiative(ids);
+      setCurrentDate(new Date());
+      start.current = new Date();
+    }
   };
 
   const handleNext = () => {
     setNextToken();
   };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentDate(new Date());
+    }, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [setCurrentDate]);
 
   return (
     <div className="flex flex-row items-center justify-around mb-2 sticky top-0 z-50 bg-base-100 p-2">
@@ -29,6 +46,19 @@ export default function Controls() {
         {!scene.inCombat && "Start"}
       </button>
       {scene.inCombat && <span>Round {scene.rounds}</span>}
+      {scene.inCombat && (
+        <span>
+          {formatDuration(
+            intervalToDuration({
+              start: start.current,
+              end: currentDate,
+            }),
+            {
+              delimiter: ", ",
+            }
+          )}
+        </span>
+      )}
       {scene.inCombat && (
         <button onClick={handleNext} className="btn btn-primary btn-sm">
           Next
